58bbce4260018987e8237b5eb06e42cf57a5dabf
jslucf <jslaso@gmail.com>
2017-02-06 11:15:11 -0500

    Update Scraping Data -- WWE Ratings.R

--- Scraping Data -- WWE Ratings.R
+++ Scraping Data -- WWE Ratings.R
@@ -1,7 +1,7 @@
-# Jason Laso
-# 2/2/17
-# Introduction to scraping & data preparation using WWE TV ratings
-
+# Jason Laso                                                        #
+# 2/2/17                                                            #
+# Introduction to scraping & data preparation using WWE TV ratings  #
+#####################################################################
 
 library(rvest)
 library(stringr)
@@ -22,6 +22,7 @@ for(i in 1:23){
   
   #assemble list of URLs
   url[i] = paste("http://wrestlingdata.com/index.php?befehl=quoten&art=2&liga=1&show=1&sort=0&seite=",i, sep="")
+  
   #read the url and isoltate the nodes
   webpage = read_html(url[i])
   htmlnodes <- html_nodes(webpage, 'table')
@@ -41,6 +42,7 @@ for(i in 1:17){
   
   #assemble list of URLs
   url[i] = paste("http://wrestlingdata.com/index.php?befehl=quoten&art=2&liga=1&show=2&sort=0&seite=",i, sep="")
+  
   #read the url and isoltate the nodes
   webpage = read_html(url[i])
   htmlnodes <- html_nodes(webpage, 'table')
@@ -78,38 +80,30 @@ ratings$viewers = as.numeric(ratings$viewers)
 str(ratings)
 summary(ratings)
 
-# We need to extract "Raw" and "Smackdown" from the event variable. This loop searches each row and assigns
-# a much simpler show name to a new column, show.
+# We need to extract "Raw" and "Smackdown" from the event variable because the shows have used different 
+# iterations of the same names over the years. This loop searches each row and assigns a much simpler 
+# show name to a new column, show.
+
 for(i in 1:nrow(ratings)){
   
   # This is searching for "RAW" or "Smack" in the event string and returning the new show column.
   
   if(grepl("RAW", ratings$event[i])){
         ratings$show[i] = "raw"
+        
   }else if(grepl("Smack", ratings$event[i])){
         ratings$show[i] = "smackdown"
+        
   }else{
         ratings$show[i] = "misc"
   }
 }
-#Check results
+
+# Check results
 table(ratings$show)
 
 # The scrape reads in an extra row, "Brawl to End it All" since it includes the word "raw". Remove that row.
 ratings = ratings[-which(ratings$show == "misc"),] 
-
-# Raw went to 3 hours on 7/23/12.
-date_filter = "2012-07-23"
-
-# Timeline of ratings of both shows since the expansion.
-ggplot(subset(ratings, date>=date_filter & viewers>1000000), aes(x=date, y=viewers, col=show)) + 
-  geom_line() + geom_smooth() + 
-  scale_y_discrete(limits=c(2e+06, 3e+06, 4e+06, 5e+06, 6e+06), 
-                   labels=c("2M", "3M", "4M", "5M", "6M")) +
-  ggtitle(paste("WWE Viewership Since", date_filter)) + 
-  theme(plot.title = element_text(family = "Trebuchet MS", color="blue", face="bold", size=32, hjust=0))
-
-
 
 # Let's write a csv to our directory with our cleaned up dataset now.
 write.csv(ratings, "wwetvratings.csv", row.names=F)
